0d0ee4ae8364d7b585ea24d90196a001eabe952d
plosnak2 <jakub.zaukolec@gmail.com>
2023-06-03 14:22:56 +0200

    defining json data

---
+++ src/data/json.ts
@@ -0,0 +1,34 @@
+import { IItem } from "../types/Cart";
+
+export const json: IItem[] = [
+    {
+        name: "Notebook",
+        price: 999,
+        image: ""
+    },
+    {
+        name: "Book",
+        price: 49,
+        image: ""
+    },
+    {
+        name: "Car",
+        price: 19999,
+        image: ""
+    },
+    {
+        name: "Ball",
+        price: 15,
+        image: ""
+    },
+    {
+        name: "Playstation 5",
+        price: 499,
+        image: ""
+    },
+    {
+        name: "IPhone 14",
+        price: 759,
+        image: ""
+    }
+]
