ca4c2de37b1e0b3a1f5fe2595183c9024ed8c65a
plosnak2 <jakub.zaukolec@gmail.com>
2023-06-03 14:52:33 +0200

    added bootstrap, created basic navbar, created icon for shopping cart with indicator

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,13 @@ import React, { useContext } from 'react';
 import ShoppingProvider from './context/shoppingContext';
 import { ShoppingContext } from './context/shoppingContext';
 import { ShoppingContextInterface } from './types/Cart';
-import { Navbar } from './components/navbar';
+import { CustomNavbar } from './components/navbar';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <ShoppingProvider>
-      <Navbar />
+      <CustomNavbar />
     </ShoppingProvider>
   );
 }
--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,12 +1,31 @@
 import { FC, useContext} from "react";
 import { ShoppingContext } from "../context/shoppingContext";
 import { ShoppingContextInterface } from "../types/Cart";
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import { FiShoppingCart } from 'react-icons/fi';
+import './navbar.css';
 
-export const Navbar: FC = ({}) => {
+export const CustomNavbar: FC = ({}) => {
     const { items, addItem } = useContext(ShoppingContext) as ShoppingContextInterface;
     return (
-        <div>
-            Navbar
-        </div>
+        <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark" style={{padding:0}}>
+            <Container>
+                <Navbar.Brand>React-Typescript-Shopping Cart</Navbar.Brand>
+                <Nav className="me-auto">
+                </Nav>
+                <Nav>
+                    <Nav.Link>
+                        <div className="cart">
+                            <FiShoppingCart size={20} color="black"/>
+                            {
+                                items.length === 0 ? null : <div className="indicator">{items.length}</div>
+                            }
+                        </div>
+                    </Nav.Link>
+                </Nav>
+            </Container>
+        </Navbar>
     );
 };
