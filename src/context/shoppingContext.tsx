1bdf6d309372d1d79dc3a6ef7c7a1c322ad948f3
plosnak2 <jakub.zaukolec@gmail.com>
2023-06-05 10:34:20 +0200

    contents of shopping cart is now stored in cookie

--- src/context/shoppingContext.tsx
+++ src/context/shoppingContext.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import { ShoppingContextInterface, IItem } from '../types/Cart';
+import Cookies from 'js-cookie';
 
 export const ShoppingContext = React.createContext<ShoppingContextInterface | null>(null);
 
@@ -8,10 +9,11 @@ interface Props {
   }
 
 const ShoppingProvider: React.FC<Props> = ({ children }) => {
-    const [items, setItems] = React.useState<IItem[]>([]);
+    const [items, setItems] = React.useState<IItem[]>(Cookies.get('items') === undefined ? [] : JSON.parse(Cookies.get('items') as string) as IItem[]);
 
     const addItem = (item: IItem) => {
         setItems([...items, item])
+        Cookies.set('items',JSON.stringify([...items, item]));
         return
     }
 
@@ -26,6 +28,7 @@ const ShoppingProvider: React.FC<Props> = ({ children }) => {
         array.splice(index, 1)
         setItems(array)
         console.log(items)
+        Cookies.set('items',JSON.stringify(array));
         return
     }
 
